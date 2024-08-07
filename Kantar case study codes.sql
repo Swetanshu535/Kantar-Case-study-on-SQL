e150109878e2b1da48a0105cb79cd7d675d77527
Swetanshu535 <swetanshu171@gmail.com>
2024-08-07 16:25:25 +0530

    Question and code file uploaded
    One file has the questions related to Kantar queries 
    Another file contains the codes

---
+++ Kantar case study codes.sql
@@ -0,0 +1,30 @@
+create database kantar;
+
+use kantar;
+
+select * from data1;
+select * from data2;
+select OrderID from data1;
+
+
+-- question 1
+select count(*)
+from data1 d1 left join data2 d2 on d1.OrderID=d2.OrderID and d1.productID=d2.ProductID
+where d2.OrderID is null and d2.ProductID is null;
+-- question 2
+select count(*)
+from data2 d2 left join data1 d1 on d1.OrderID=d2.OrderID and d1.productID=d2.ProductID
+where d1.OrderID is null and d1.ProductID is null;
+-- question 3
+select sum(d2.qty)
+from data2 d2 left join data1 d1 on d1.OrderID=d2.OrderID and d1.productID=d2.ProductID
+where d1.OrderID is null and d1.ProductID is null;
+-- question 4
+select count(*)
+from 
+(
+select orderID,ProductID
+from data1
+union
+select orderID,ProductID
+from data2) as temptable;
---
+++ Kantar case study questions.sql
@@ -0,0 +1,11 @@
+/*Q1: 1) How to identify the Records (Order ID + Product ID combination) 
+present in data1 but missing in data2 (Specify the number of records missing in your answer)
+*/
+/*Q2: How to identify the Records (Order ID + Product ID 
+combination) missing in data1 but present in data2 
+(Specify the number of records missing in your answer)*/
+/*Q3. Find the Sum of the total Qty of Records 
+missing in data1 but present in data2*/
+/*Q4. Find the total number of unique records 
+(Order ID + Product ID combination) present in 
+the combined dataset of data1 and data2*/
